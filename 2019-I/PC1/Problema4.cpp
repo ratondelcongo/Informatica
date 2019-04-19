d8d3006474d5af3f61c2a75134bede5bb095bd44
Luigi Layme <35671380+ratondelcongo@users.noreply.github.com>
2019-04-19 15:21:47 -0500

    Update Problema4.cpp

--- 2019-I/PC1/Problema4.cpp
+++ 2019-I/PC1/Problema4.cpp
@@ -1,31 +1,36 @@
- #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
     
-    int n, c = 0;
-    cout << "dijite un numero:" << endl;;
-    cin >> n;
+    int N;
+    cin >> N;
     
-    do{
-        int aux = n, inverso = 0, cifra;
+    for(int i = 0; i < N; i++){
+        
+        int n, c = 0;
+        
+        cin >> n;
+        
+        do{
+            int aux = n, inverso = 0, cifra;
 
-        while( n > 0 ) {    // con este while invertimos el numero
-            cifra = n % 10;
-    
-            inverso = inverso * 10 + cifra;
-            n = n / 10;     // tambien se puede escribir n /= 10;
-        }
-        n = inverso;        // numero igual a su inverso
-        if(n == aux)
-            break;          
-        n += aux;
-           // sumamos el original almacenado en aux
-        c++;
-    }while(c < 100);     // si el contador es menor a 100
-                         // el do-while se seguira ejecutando
-    cout << n << endl;
-    
+            while( n > 0 ) {    // con este while invertimos el numero
+                cifra = n % 10;
+        
+                inverso = inverso * 10 + cifra;
+                n = n / 10;     // tambien se puede escribir n /= 10;
+            }
+            n = inverso;        // numero igual a su inverso
+            if(n == aux)
+                break;          
+            n += aux;
+            // sumamos el original almacenado en aux
+            c++;
+        }while(c < 100);     // si el numero no es capicula Y el contador es menor a 100
+                                            // el do-while se seguira ejecutando
+        cout << n << endl;
+    }   
     return 0;
 }    
