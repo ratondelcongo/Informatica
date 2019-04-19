f4fd20dddf4f25d6d50991c7ceaa430108e594bc
Luigi Layme <35671380+ratondelcongo@users.noreply.github.com>
2019-04-19 15:17:55 -0500

    Create Problema4.cpp

---
+++ 2019-I/PC1/Problema4.cpp
@@ -0,0 +1,31 @@
+ #include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    int n, c = 0;
+    cout << "dijite un numero:" << endl;;
+    cin >> n;
+    
+    do{
+        int aux = n, inverso = 0, cifra;
+
+        while( n > 0 ) {    // con este while invertimos el numero
+            cifra = n % 10;
+    
+            inverso = inverso * 10 + cifra;
+            n = n / 10;     // tambien se puede escribir n /= 10;
+        }
+        n = inverso;        // numero igual a su inverso
+        if(n == aux)
+            break;          
+        n += aux;
+           // sumamos el original almacenado en aux
+        c++;
+    }while(c < 100);     // si el contador es menor a 100
+                         // el do-while se seguira ejecutando
+    cout << n << endl;
+    
+    return 0;
+}    
